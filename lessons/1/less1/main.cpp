3bc89663398d9fe45bbe4a0e822db2aacce387a8
fractal.ai <96378549+icosahedron-math@users.noreply.github.com>
2021-12-25 15:58:11 +0300

    first main programm

---
+++ lessons/1/less1/main.cpp
@@ -0,0 +1,18 @@
+////////////////
+// First code //
+////////////////
+
+#include "process.h"
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "Hell World\n"; // std::cout write text to terminal
+
+    // make short variable
+    short a = 5;
+    a++; // a + 1
+    std::cout << a; // std::cout a
+
+    return 0;
+}
